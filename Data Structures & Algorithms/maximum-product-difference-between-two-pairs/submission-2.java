f5f6d21fc0a7b7215c46503dfc6fb4a0e5d2b402
ar00ster18 <71556202+ar00ster18@users.noreply.github.com>
2026-05-16 18:06:52 +0300

    Add: maximum-product-difference-between-two-pairs - submission-2

---
+++ Data Structures & Algorithms/maximum-product-difference-between-two-pairs/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxProductDifference(int[] nums) {
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        
+        for (int n : nums) {
+            // update maximums
+            if (n >= max1) {
+                max2 = max1;
+                max1 = n;
+            } else if (n > max2) {
+                max2 = n;
+            }
+            
+            // update minimums
+            if (n <= min1) {
+                min2 = min1;
+                min1 = n;
+            } else if (n < min2) {
+                min2 = n;
+            }
+        }
+        
+        return (max1 * max2) - (min1 * min2);
+        }
+}
